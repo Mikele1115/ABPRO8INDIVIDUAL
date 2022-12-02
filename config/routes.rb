964d47b5a860530dfe692599aece8eb05155e421
Michele <michele.bavestrello@gmail.com>
2022-12-01 21:03:55 -0300

    creacion de proyecto y vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
-# config/routes.rb
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # config/routes.rb
 get '/home',        to: 'pages#home'
 get '/contact',     to: 'pages#contact'
 root 'pages#home'
+end
